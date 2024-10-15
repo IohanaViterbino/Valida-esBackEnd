7215237116f6a6c567b08f2a6270af00c96a00f4
Iohana <iohana.ma31@gmail.com>
2024-10-15 16:53:37 -0300

    refactor: add validators in serializers and models

--- escola/serializers.py
+++ escola/serializers.py
@@ -1,10 +1,30 @@
 from rest_framework import serializers
-from escola.models import Estudante,Curso, Matricula
+from validate_docbr import CPF
+import re, datetime
+from escola.models import Estudante, Curso, Matricula
 
 class EstudanteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Estudante
-        fields = ['id','nome','email','cpf','data_nascimento','celular']
+        fields = ['id','nome','email','cpf','data_nascimento','celular']    
+    
+    def validate(self, dados):
+        cpf = CPF()
+        regex_email = re.compile(r'[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?', re.IGNORECASE)
+        regex_nome = re.compile(r'^[a-zA-Z\s]*$', re.IGNORECASE)
+
+        if re.match(regex_email, dados['email']) == None:
+            raise serializers.ValidationError({'email':'O email não está no formato correto!'})
+        if dados['data_nascimento'] > datetime.date.today():
+            raise serializers.ValidationError({'data_nascimento':'A data de nascimento não pode ser maior que a data atual!'})
+        if len(dados['celular']) != 13:
+            raise serializers.ValidationError({'celular':'O celular precisa ter 13 digitos!'})
+        if re.match(regex_nome, dados['nome']) == None:
+            raise serializers.ValidationError({'nome':'O nome deve conter apenas letras!'})
+        if not cpf.validate(dados['cpf']):
+            raise serializers.ValidationError({'cpf':'O cpf deve ter 11 digitos numéricos válidos!'})
+        return dados
+
 
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
